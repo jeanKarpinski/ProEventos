f00da4bc6e7a809d9d5f1dc777a71637792fec7e
Jean Carlos <jcrk22@gmail.com>
2023-05-25 20:21:33 -0300

    Adicionado NO_ERRORS_SCHEMA em app.module.ts, pois estava sendo apresentado o seguinte erro: Can't bind to 'ngModel' since it isn't a known property of 'input'

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -30,5 +30,6 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  //schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule {}

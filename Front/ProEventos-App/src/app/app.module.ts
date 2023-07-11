dbaf7131af1fe70c832097873ddbe20da8ec4fa6
Jean Carlos <jcrk22@gmail.com>
2023-07-10 21:09:54 -0300

    Seção 7: Angular - Organizando, Rotas, Alertas e +

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,24 +1,43 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { NavComponent } from './shared/nav/nav.component';
+import {TituloComponent} from './shared/titulo/titulo.component'
 
-import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { EventoService } from './services/Evento.service';
 
+import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
     PalestrantesComponent,
+    ContatosComponent,
+    DashboardComponent,
+    PerfilComponent,
     NavComponent,
+    TituloComponent,
+    DateTimeFormatPipe,
   ],
   imports: [
     BrowserModule,
@@ -27,9 +46,19 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+    }),
+    NgxSpinnerModule
   ],
-  providers: [],
+  providers: [EventoService],
   bootstrap: [AppComponent],
-  schemas: [NO_ERRORS_SCHEMA]
+  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
---
+++ Front/ProEventos-App/src/app/services/Evento.service.ts
@@ -0,0 +1,24 @@
+import { Evento } from '../models/Evento';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+
+export class EventoService {
+  baseURL = 'http://localhost:5000/api/eventos';
+
+  constructor(private http: HttpClient) {}
+
+  public getEventos(): Observable<Evento[]> {
+    return this.http.get<Evento[]>(this.baseURL);
+  }
+
+  public getEventosByTema(tema: string): Observable<Evento[]> {
+    return this.http.get<Evento[]>(`${this.baseURL}/${tema}/tema`);
+  }
+
+  public getEventoById(id: number): Observable<Evento[]> {
+    return this.http.get<Evento[]>(`${this.baseURL}/${id}`);
+  }
+}
